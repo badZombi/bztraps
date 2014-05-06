d5876a3000c899f8c2049135001c1a384281fc44
John Lund <git@badzombi.com>
2014-05-06 05:01:45 -0400

    wtf fix

--- BZtraps.js
+++ BZtraps.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-// BZtraps Magma Plugin v 0.1 by BadZombi
-
-function On_EntityHurt(he) {
-    try{
-	    if(he.Attacker == he.Entity.Owner){
-		    if(he.Entity.Name == "WoodCeiling" || he.Entity.Name == "MetalCeiling"){
-		    	var pendingtrap = DataStore.Get(he.Attacker.SteamID, "BZTpending");
-		    	if(pendingtrap != undefined && pendingtrap != "none"){
-			    	he.Attacker.Message("Trap set on trigger door : " + pendingtrap + ". ");
-			    	DataStore.Add("BZtraps", pendingtrap, he.Entity);
-					Datastore.Remove(he.Attacker.SteamID, "BZTpending");
-					DataStore.Remove(he.Attacker.SteamID, "BZpending");
-					DataStore.Save();	
-				}
-		    }
-		}
-	} catch(err){
-		he.Attacker.Broadcast("Error: " + err.message);
-		he.Attacker.Broadcast("Details: " + err.description);
-	}
-=======
 // Set traps in rust!
 
 var BZT = {
@@ -82,7 +60,7 @@ var BZT = {
 		DataStore.Save();
 	}
 }
- 
+
 
 
 // Hooks:
@@ -168,44 +146,17 @@ function On_EntityHurt(he) {
 		Server.Broadcast("Error Message: " + err.message);
 		Server.Broadcast("Error Description: " + err.description);
 	}    
->>>>>>> 304572160b6d284cfb976bf55c8c5918010f8327
 }
 
 function On_DoorUse(Player, de) {
     
-<<<<<<< HEAD
-=======
     var doorID = HASH.get(de.Entity.X +"|"+ de.Entity.Y +"|"+ de.Entity.Z);
     
->>>>>>> 304572160b6d284cfb976bf55c8c5918010f8327
     if(Player == de.Entity.Owner) {
         
         var pendingtrap = DataStore.Get(Player.SteamID, "BZpending");
 
         if(pendingtrap != undefined && pendingtrap == "setTrap"){
-<<<<<<< HEAD
-			DataStore.Add(Player.SteamID, "BZTpending", de.Entity.InstanceID);
-			Player.Message("Trap will be set on door: " + de.Entity.InstanceID + ". ");
-			Player.Message("Now hit the floor outside the door to mark it.");
-
-		} 
-
-        return;
-    } else {
-    	var trapped = DataStore.Get("BZtraps", de.Entity.InstanceID);
-    	if(trapped != undefined){
-    		Player.Notice("Aaaah! Its a Trap!");
-    		
-    		trapped.Destroy();
-    		//trapped.X = trapped.X + 10;
-    		DataStore.Remove("BZtraps", de.Entity.InstanceID);
-			DataStore.Save();
-    	}
-    }
-}
-
-
-=======
 
         	var existingcheck = DataStore.Get(BZT.DStable, doorID);
 
@@ -319,24 +270,12 @@ function On_DoorUse(Player, de) {
     }
 }
 
->>>>>>> 304572160b6d284cfb976bf55c8c5918010f8327
 function On_Command(Player, cmd, args) { 
 
 	cmd = Data.ToLower(cmd);
 	switch(cmd) {
 
 		case "trap":
-<<<<<<< HEAD
-			var pendingtrap = DataStore.Get(Player.SteamID, "BZpending");
-
-			if(pendingtrap != undefined && pendingtrap == "setTrap"){
-				DataStore.Add(Player.SteamID, "BZpending", "none");
-				Player.Message("Canceled trap setting.");
-			} else {
-				DataStore.Add(Player.SteamID, "BZpending", "setTrap");
-				Player.Message("The next door (which you own) you open will be trapped...");
-				// start a timer so this will expire and notify the user.
-=======
 			
 			if(args.Length == 1){
 				var type = Data.ToLower(args[0]);
@@ -383,7 +322,6 @@ function On_Command(Player, cmd, args) {
 				Player.MessageFrom("Traps", "\"/trap cancel\" will cancel any trap setting.");
 				Player.MessageFrom("Traps", "---------------------------------------------");
 				break;
->>>>>>> 304572160b6d284cfb976bf55c8c5918010f8327
 			}
 
 		break;
@@ -391,9 +329,6 @@ function On_Command(Player, cmd, args) {
 		
 		
     }
-<<<<<<< HEAD
-}
-=======
 }
 
 // Callbacks:
@@ -427,4 +362,3 @@ function removeSpikesCallback(){
 
 		
 }
->>>>>>> 304572160b6d284cfb976bf55c8c5918010f8327
